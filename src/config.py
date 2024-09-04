ca7db0303fdb5630ceea34457fe3008205abb62f
Raphael Golubev <raphael.golubev@gmail.com>
2024-09-05 00:22:38 +0300

    marked all classes as private

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
 from pydantic_settings import (
-    BaseSettings, SettingsConfigDict,
+    BaseSettings, SettingsConfigDict
 )
+from pydantic import computed_field
 
 
-class ModelConfig:
+class _ModelConfig:
     """
     Позволяет иметь общий конфиг для всех моделей
     с возможностью переопределения ключей индивидуально
@@ -21,12 +22,28 @@ class ModelConfig:
         return config
 
 
-class AppSettings(BaseSettings):
+class _AppSettings(BaseSettings):
     debug: bool = False
     host: str = 'asfmlmkadsmflkds'
     port: int = 5000
 
-    model_config = ModelConfig(env_prefix="APP_")
+    model_config = _ModelConfig(env_prefix="APP_")
 
 
-app = AppSettings()
+class _DatabaseSettings(BaseSettings):
+    user: str = 'postgres'
+    password: str = 'postgres'
+    name: str = 'postgres'
+    host: str = 'postgres'
+    port: int = 5432
+
+    @computed_field
+    @property
+    def postgres_dsn(self) -> str:
+        return f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}'
+
+    model_config = _ModelConfig(env_prefix="DB_")
+
+
+app_settings = _AppSettings()
+db_settings = _DatabaseSettings()
